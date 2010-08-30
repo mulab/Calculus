f33a5a72215e72012d8c3cac4fdb41f3bd3e937e
Junlin Zhang <sortileger@gmail.com>
2010-08-30 15:38:06 +0000

    [SRT-INT]intSubTri方法重写。现在可基本上处理所有的三角函数转换

--- intSubTri.m
+++ intSubTri.m
@@ -7,7 +7,7 @@ linear[f_,x_,a_,b_]:=Module[
 	_Plus,(*the form is m*x+n*)
 	If[!FreeQ[temp=Level[f,1][[1]],x],(*temp=m*x*)
 		If[m=!=Null,
-			If[m!=temp/x||n!=f-temp,
+			If[m=!=temp/x||n=!=f-temp,
 				Return[]
 			],
 			m=temp/x;
@@ -21,7 +21,7 @@ linear[f_,x_,a_,b_]:=Module[
 		],
 		(*temp=n*)
 		If[m=!=Null,
-			If[n!=temp||m!=(f-temp)/x,
+			If[n=!=temp||m=!=(f-temp)/x,
 				Return[]
 			],
 			n=temp;
@@ -34,7 +34,7 @@ linear[f_,x_,a_,b_]:=Module[
 	Return[{m,n}],
 	_,(*the form is m*x or x*)
 	If[n=!=Null,
-		If[n!=0||m!=f/x,
+		If[n=!=0||m=!=f/x,
 			Return[]
 		],
 		n=0
@@ -51,7 +51,7 @@ linear[f_,x_,a_,b_]:=Module[
 (*The integrand is an elementary function of the trigonometric function applied to linear argument in the variable of integration.*)
 intSubTri[f_,x_]:=Catch[
 	Module[
-	{e=f,pos,loci,triArg,triFun={{},{},{},{},{},{}},a=Null,b=Null,n,temp,s,trigList,i,y,t},
+	{e=f,pos,loci,triArg,triFun={{},{},{},{},{},{}},a=Null,b=Null,t,et,temp,i,j,k,trigList,s,n,sin,cos,tan,sec,y},
 	tri={Sin,Cos,Tan,Cot,Sec,Csc};
 	Do[
 		pos=Position[e,tri[[k]][_]];(*gives the position of Trig (ax+b)*)
@@ -71,81 +71,81 @@ intSubTri[f_,x_]:=Catch[
 		e=ReplacePart[e,triFun[[k]]->tri[[k]][t]],(*substitute Trig (ax+b) to Trig (t)*)
 		{k,Length[tri]}
 	];
-	If[triFun[[1]]=={}&&triFun[[2]]=={},
-		Throw["NotMatch"]
-	];                                                           (*\:6743\:5b9c\:4e4b\:7b56\:ff0c\:6b64\:65b9\:6cd5\:4ee5\:540e\:5c1a\:9700\:5927\:7684\:6539\:8fdb*)
-	(*Now has got all the trig information of f*)
-	(*transform all trigonometric functions into sines and cosines,not done...*)
-
-	(*transform the form Sin[x]^m*Cos[x]^n:NEEDED?*)
 	
-	(*cases that can substitute z=cosy or z=siny*)
-	If[Head[e]===Times,
-		temp=Part[e,1];
-		trigList={Sin,Cos};
-		For[i=1,i<3,i++,
-		If[MatchQ[temp,trigList[[i]][t]^_Integer] && OddQ[Part[temp,2]],
-			n=(Part[temp,2]-1)/2;
-			temp=e/temp,
-			If[MatchQ[e/temp,trigList[[i]][t]^Integer] && OddQ[Part[e/temp,2]],
-				n=(Part[e/temp,2]-1)/2,
-				If[MatchQ[temp,trigList[[i]][t]],
-					n=0;
-					temp=e/temp,
-					If[MatchQ[e/temp,trigList[[i]][t]],
-						n=0,
-						Continue[]
+	(*All trigonometric functions transformed into sines and cosines*)
+	et=ReplacePart[e,{triFun[[1]]->sin,triFun[[2]]->cos,triFun[[3]]->sin/cos,triFun[[4]]->cos/sin,triFun[[5]]->1/cos,triFun[[6]]->1/sin}];
+	
+	If[Head[et]===Times,
+		parts=Level[et,1];
+		trigList={sin,cos};
+		For[j=1,j<=Length[parts],j++,(*outer for cycle*)
+			temp=Part[et,j];	
+			For[i=1,i<=Length[trigList],i++,(*inner for cycle*)
+				If[MatchQ[temp,trigList[[i]]^_Integer] && OddQ[Part[temp,2]],
+					n=(Part[temp,2]-1)/2;
+					temp=et/temp,
+					If[MatchQ[temp,trigList[[i]]],
+						n=0;
+						temp=et/temp,
+						Continue[](*next inner for cycle step*)
 					]
+				];
+				(*now temp is supposed to be Elem (sint^2,cost) or Elem (cost^2,sint)*)
+				pos=Position[temp,trigList[[i]]];
+				If[Catch[
+					Do[
+						loci=pos[[k]];
+						If[Head[s=Extract[temp,Delete[loci,-1]]]===Power&&Part[s,2]===2,
+							temp=ReplacePart[temp,Delete[loci,-1]->1-y^2],
+							Throw["break"]
+						],
+						{k,Length[pos]}
+					]
+					]==="break",
+					Break[];(*inner for cycle break*)
 				]
-			]
-		];(*now temp is Elem (sint^2,cost) or Elem (cost^2,sint)*)
-		pos=Position[temp,trigList[[i]][t]];
+
+				If[i==1,
+					temp=ReplacePart[temp,Position[temp,cos]->y];
+					Throw[{-(1-y^2)^n*temp/a,y,Cos[a*x+b]}],
+					temp=ReplacePart[temp,Position[temp,sin]->y];
+					Throw[{(1-y^2)^n*temp/a,y,Sin[a*x+b]}]
+				]
+			](*inner for cycle end*)
+		](*outer for cycle end*)
+	];
+
+	(*All trigonometric functions transformed into secants and tangents*)
+	et=ReplacePart[e,{triFun[[1]]->tan/sec,triFun[[2]]->1/sec,triFun[[3]]->tan,triFun[[4]]->1/tan,triFun[[5]]->sec,triFun[[6]]->sec/tan}];
+	pos=Position[et,sec];
+	If[Catch[
 		Do[
-			loci=pos[[i]];
-			If[Head[s=Extract[temp,Delete[loci,-1]]]===Power&&Part[s,2]===2,
-				temp=ReplacePart[temp,Delete[loci,-1]->1-y^2]
+			loci=pos[[k]];
+			If[Head[s=Extract[et,Delete[loci,-1]]]===Power&&Part[s,2]===2,(*Of the form Elem (tant,sect^2)*)
+				et=ReplacePart[et,Delete[loci,-1]->1+y^2],
+				Throw["break"]
 			],
-			{i,Length[pos]}
-		];
-		If[i==1,
-			temp=ReplacePart[temp,Position[temp,Cos[t]]->y];
-			Throw[{-(1-y^2)^n*temp/a,y,y}],                                    (*\:6b64\:5904\:5047\:5b9a\:8fd4\:56de\:4ecd\:4e3ay,\:53ea\:4e3a\:8c03\:8bd5*)
-			temp=ReplacePart[temp,Position[temp,Sin[t]]->y];
-			Throw[{(1-y^2)^n*temp/a,y,y}]                                       (*\:540c\:4e0a*)
+			{k,Length[pos]}
 		]
-		](*For cycle end*)
+		]=!="break",
+		et=ReplacePart[et,Position[et,tan]->y];
+		Throw[{1/(1+y^2)*et/a,y,Tan[a*x+b]}]
 	];
-
-	(*Cases that can substitute z=tany*)
-	If[triFun[[1]]=={}&&triFun[[2]]=={}&&triFun[[4]]=={}&&triFun[[6]]=={},(*to transform to Elem (tant,sec^2 t   and temp=f*)
-		temp=e;
-		pos=Position[temp,Sec[t]];
-		Do[
-			loci=pos[[i]];
-			If[Head[s=Extract[temp,Delete[loci,-1]]]===Power&&Part[s,2]===2,
-				temp=ReplacePart[temp,Delete[loci,-1]->1+y^2]
-			],
-			{i,Length[pos]}
-		];
-		temp=ReplacePart[temp,Position[temp,Tan[t]]->y];
-		Throw[{1/(1+y^2)*temp/a,y,y}];                                            (*\:540c\:4e0a*)
-	];
-	(*substitute z=tan1/2*y*)		
-	If[triFun[[1]]=!={},e=ReplacePart[e,triFun[[1]]->2y/(1+y^2)]];
-	If[triFun[[2]]=!={},e=ReplacePart[e,triFun[[2]]->(1-y^2)/(1+y^2)]];
-	If[triFun[[3]]=!={},e=ReplacePart[e,triFun[[3]]->2y/(1-y^2)]];
-	If[triFun[[4]]=!={},e=ReplacePart[e,triFun[[4]]->(1-y^2)/(2y)]];
-	If[triFun[[5]]=!={},e=ReplacePart[e,triFun[[5]]->(1+y^2)/(1-y^2)]];
-	If[triFun[[6]]=!={},e=ReplacePart[e,triFun[[6]]->(1+y^2)/(2y)]];
-	Throw[{Simplify[2/(1+y^2)*e/a],y,y}];                                                (*\:540c\:4e0a*)
-
-	Throw["NotMatch"](*!!Need improve,return transformed expressions,so are the aboves*)
-
-]
-]
-	
+	(*substitute z=tan1/2*y*)
+	et=ReplacePart[e,{triFun[[1]]->2y/(1+y^2),triFun[[2]]->(1-y^2)/(1+y^2),triFun[[3]]->2y/(1-y^2),triFun[[4]]->(1-y^2)/(2y),triFun[[5]]->(1+y^2)/(1-y^2),triFun[[6]]->(1+y^2)/(2y)}];
+	Throw[{Simplify[2/(1+y^2)*et/a],y,Tan[1/2*(a*x+b)]}];
+	Throw["NotMatch"]
+	]
+]	
 
 
+(*Examples
+intSubTri[Sin[t+r x]*Tan[r x+t],x]
+intSubTri[(Sin[t+r x])*Tan[r x+t]/(1+Cot[r x+t]/Csc[r x+t]),x]
+intSubTri[(Sin[t+r x])^2*Tan[r x+t]/(1+Cot[r x+t]/Csc[r x+t]),x]
+intSubTri[(1+Tan[t+r x])*(Sec[t+r x])^2,x]
+*)
 
 
-
+(* ::InheritFromParent:: *)
+(**)
