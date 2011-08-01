d6695bd37c7d543e44e998e02ad7c82849ba8db8
Wanli Ma <ma_wan_li.6209@163.com>
2011-08-01 11:01:56 +0000

    [INT]Revised by ivan on Aug 1st,2011 In intSubBin.m: change return value to {expr,y,yx}

--- Albi/intSubBin.m
+++ Albi/intSubBin.m
@@ -1,5 +1,11 @@
 (* ::Package:: *)
 
+(* Method 8 in Stage II, (4)Chebyshev *)
+(* Last revised by ivan on 18:49 Aug 1, 2011. Email: ma_wan _li .6209@163.com *)
+(* I ONLY changed the following part of the original code:
+  a)interface of this function: revise return to {expr,y,yx} in which y is the variable of expr and yx is the relationship between y and x
+*)
+
 (*aaThe integrand is not a rational function and possesses the form ax^r*(c1+c2*x^q)*p,
 where A,c1,c2 are constants,p,q,r are rational numbers and c1*c2*q*p!=0*)
 intSubBin[f_,x_]:=Module[
@@ -63,36 +69,39 @@ intSubBin[f_,x_]:=Module[
 		If[Head[r1]===Integer&&r2>0,(*substitute y=c1+c2*x^q*)
 			e=e/(c2*q*x^(q-1));
 			e=e/.x->((y-c1)/c2)^(1/q);
-			Return[{Simplify[e],y}]
+			Return[{Simplify[e],y,c1+c2*x^q}]
 		];
 		If[Head[r2]===Integer&&Head[r1]===Rational,(*substitute y=x^(q/d1),d1 is the denominator of r1*)
 			d=Denominator[r1];
 			e=e/(q/d*x^(q/d-1));
 			e=e/.x->y^(d/q);
-			Return[{Simplify[e],y}]
+			Return[{Simplify[e],y,x^(q/Denominator[r1])}]
 		];
 		If[Head[r1]===Integer&&r1<0&&Head[r2]===Rational,(*substitute y=(c1+c2*x^q)^(1/d2),d2 is the denominator of r2*)
 			d=Denominator[r2];
 			e=e/((c2*q/d)*x^(q-1)*(c1+c2*x^q)^(1/d-1));
 			e=e/.x->((y^d-c1)/c2)^(1/q);
-			Return[{Simplify[e],y}]
+			Return[{Simplify[e],y,(c1+c2*x^q)^(1/Denominator(r2))}]
 		];
 		If[Head[r1+r2]===Integer,(*substitute y=((c1+c2*y)/y)^(1/d1),d1 is the denominator of r1*)
 			d=Denominator[r1];
 			e=e/((-q*c1/d)*(c1/x^q+c2)^(1/d-1)/x^(q+1));
 			e=e/.x->(c1/(y^d-c2))^(1/q);
-			Return[{Simplify[e],y}]
+			Return[{Simplify[e],y,((c1+c2*x^q)/x^q)^(1/Denominator[r1])}]
 		],
 		Return["NotMatch"]
 	]
 ]
+assertion
 
 
-
-(*Test:
+(*Test:*)
 intSubBin[x^4*(1-x^2)^(-5/2),x]
 intSubBin[x^(1/2)*(1+x)^(5/2),x]
-*)
+
+
+
+
 
 
 
