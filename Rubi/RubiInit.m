5a2b113c2a1fe148b3942f5fb34e25f9c1f76cc0
Dejun Lin <buptldj@163.com>
2011-08-01 11:18:47 +0000

    [INT] 将Rubi中的所有“。m”文件装载在一起

---
+++ Rubi/RubiInit.m
@@ -0,0 +1,71 @@
+(* ::Package:: *)
+
+SetDirectory[NotebookDirectory[]];
+Import["original\\AlgebraicFunctions\\AlgebraicFunctionsOfBinomials.m"];
+Import["original\\AlgebraicFunctions\\AlgebraicFunctionsOfLinears.m"];
+Import["original\\AlgebraicFunctions\\AlgebraicFunctionsOfTrinomials.m"];
+Import["original\\ExponentialFunctions\\ExponentialFunctions.m"];
+Import["original\\ExponentialFunctions\\LogarithmFunctions.m"];
+Import["original\\ExponentialFunctions\\ProductsOfExponentialAndHyperbolicFunctions.m"];
+Import["original\\ExponentialFunctions\\ProductsOfExponentialAndTrigFunctions.m"];
+Import["original\\HyperbolicFunctions\\HyperbolicSecantFunctions.m"];
+Import["original\\HyperbolicFunctions\\HyperbolicSineFunctions.m"];
+Import["original\\HyperbolicFunctions\\HyperbolicSubstitution.m"];
+Import["original\\HyperbolicFunctions\\HyperbolicTangentFunctions.m"];
+Import["original\\HyperbolicFunctions\\RationalFunctionsOfHyperbolicSines.m"];
+Import["original\\HyperbolicFunctions\\RationalFunctionsOfHyperbolicSinesAndCosines.m"];
+Import["original\\HyperbolicFunctions\\TwoHyperbolicFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicCosecantFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicCosineFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicCotangentFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicSecantFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicSineFunctions.m"];
+Import["original\\InverseHyperbolicFunctions\\InverseHyperbolicTangentFunctions.m"];
+Import["original\\InverseTrigFunctions\\InverseCosecantFunctions.m"];
+Import["original\\InverseTrigFunctions\\InverseCosineFunctions.m"];
+Import["original\\InverseTrigFunctions\\InverseCotangentFunctions.m"];
+Import["original\\InverseTrigFunctions\\InverseSecantFunctions.m"];
+Import["original\\InverseTrigFunctions\\InverseSineFunctions.m"]; 
+Import["original\\InverseTrigFunctions\\InverseTangentFunctions.m"];
+Import["original\\RationalFunctions\\RationalFunctionsOfBinomials.m"];
+Import["original\\RationalFunctions\\RationalFunctionsOfLinears.m"];
+Import["original\\RationalFunctions\\RationalFunctionsOfTrinomials.m"];
+Import["original\\SpecialFunctions\\ErrorFunctions.m"];
+Import["original\\SpecialFunctions\\ExponentialIntegralFunctions.m"];
+Import["original\\SpecialFunctions\\FresnelIntegralFunctions.m"];
+Import["original\\SpecialFunctions\\GammaFunctions.m"];
+Import["original\\SpecialFunctions\\HyperbolicIntegralFunctions.m"];
+Import["original\\SpecialFunctions\\LogarithmIntegralFunctions.m"];
+Import["original\\SpecialFunctions\\PolylogarithmFunctions.m"];
+Import["original\\SpecialFunctions\\ProductLogarithmFunctions.m"];
+Import["original\\SpecialFunctions\\TrigIntegralFunctions.m"];
+Import["original\\SpecialFunctions\\ZetaFunctions.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (a+a sin^k)^n.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (A+B sin^k) (a+a sin^k)^n.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (A+B sin^k) (a+b sin^k)^n.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (a+b sin^k)^n.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (A+B sin^k+C sin^(2k)) (a+a sin^k)^n.m"];
+Import["original\\TrigFunctions\\(sin^j)^m (A+B sin^k+C sin^(2k)) (a+b sin^k)^n.m"];
+Import["original\\TrigFunctions\\DomainMapOfIntegrationRules.m"];
+Import["original\\TrigFunctions\\RationalFunctionsOfSinesAndCosines.m"];
+Import["original\\TrigFunctions\\SecantFunctions.m"];
+Import["original\\TrigFunctions\\SineFunctions.m"];
+Import["original\\TrigFunctions\\TangentFunctions.m"];
+Import["original\\TrigFunctions\\TrigNormalization.m"];
+Import["original\\TrigFunctions\\TrigSubstitution.m"];
+Import["original\\TrigFunctions\\TwoTrigFunctions.m"];
+Import["original\\GeneralIntegrationRules.m"];
+Import["original\\IntegrationUtilityFunctions.m"];
+(*Import["TableInt.m"];*)
+
+
+(*
+Int[E^x,x]
+Int[x^4*(1-x^2)^(-5/2),x]
+Int[x^(1/2)*(1+x)^(5/2),x]
+Int[2,x] 
+*)
+
+
+(* ::InheritFromParent:: *)
+(* *)
