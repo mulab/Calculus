bfd3f2beac3d3d89c313d28ad5fa7a85bec7b675
Qiming Shao <shaoqiming8@gmail.com>
2011-08-03 07:50:46 +0000

    [INT] 更正a^(x^2)匹配的BUG

--- Albi/intSubExp.m
+++ Albi/intSubExp.m
@@ -7,7 +7,8 @@ Subscript[a, i]^Subscript[c, i]*Subscript[a, 1]^(Subscript[b, i]x*Log[Subscript[
 (*the method 1 of SIN ( Symbolic INtegrator ) stage II*)
 (*Shao Qiming & Zhang Junlin*)
 intSubExp[f_,x_]:=Module[
-	{e=f,pos=Position[f,x],loc={},i,j,y,len,ptr,mp,a={},b={},c={},ans},
+	{e=f,pos=Position[f,x],loc={},i,j,len,ptr,mp,a={},b={},c={},ans},
+	If[Length[pos]==0,Return["NotMatch"]];
 	ans=Do[(*enumerate each occrurance of x to get Subscript[a, i], Subscript[b, i], Subscript[c, i] and the location*)
 		ptr=pos[[i]];
 		j=ptr[[-1]];
@@ -44,9 +45,11 @@ intSubExp[f_,x_]:=Module[
 			(*is 0*)
 			AppendTo[c,0]
 		];
+		(*Extract\:4e0d\:652f\:6301Extract[expr,{}]*)
 		If[Head[Extract[e,{ptr}][[1]]]=!=Power||j!=2(*b*x+c is not the second part*),
 			Return["NotMatch"]
 		];
+		(*If[b[[-1]]==0&&c[[-1]]==1,b=Delete[b,-1];c=Delete[c,-1];Print["xxx"];Continue[]];*)
 		AppendTo[a,Extract[e,Append[ptr,1]]];
 		If[!FreeQ[a[[i]],x],
 			Return["NotMatch"]
@@ -54,10 +57,12 @@ intSubExp[f_,x_]:=Module[
 		AppendTo[loc,ptr],
 		{i,Length[pos]}
 	];
-	If[Length[pos]==0,Return["NotMatch"]];
+	(*Print[{a,b,c}];*)
 	If[ans!=Null,
 		Return[ans]
 	];
+	(*Although I do not know what's meaning of below sentence, it is geilivable*)
+	If[Length[a]!=Length[b]||Length[a]!=Length[c]||Length[b]!=Length[c],Return["NotMatch"]];
 	Do[(*construct the rule list*)
 		loc[[i]]=Rule[loc[[i]],a[[i]]^c[[i]]*y^(b[[i]]*Log[a[[1]],a[[i]]])],
 		{i,Length[pos]}
@@ -83,8 +88,7 @@ intSubExp[f_,x_]:=Module[
 	e=e/(u Log[a[[1]]]);
 	Print[u];
 	Return[Integrate[e,y]//.u->a[[1]]^x]*)
-];
-
+]
 
 
 (*intSubExp[E^x/(2+3E^(2x)),x]
@@ -92,9 +96,12 @@ intSubExp[E^(2x)/(a+b E^(4x)),x]
 intSubExp[E^(x+1)/(1+E^x),x]
 intSubExp[10^x E^x,x]
 intSubExp[(a+b^(c x+h))/(d+e^(f x+g)),x]
-intSubExp[a^x/b^x,x]
-intSubExp[x/(1+E^x),x]
+intSubExp[a^x/b^x,x]*)
+(*intSubExp[x/(1+E^x),x]
 intSubExp[x/(1+x),x]
-intSubExp[x,x]
-\:8be5\:51fd\:6570\:53ea\:8981\:5b58\:5728a^(b x+c)\:ff0c\:5c31\:4f1a\:5904\:7406\:ff0c\:9664\:6b64\:4e4b\:5916\:ff0c\:4e0d\:4f1a\:5bf9\:5176\:5b83\:7c7b\:578b\:7684x\:6240\:7ed9\:7684\:4fe1\:606f\:7ed9\:51fa\:591a\:4f59\:7684\:5224\:65ad
-*)
+intSubExp[1/1+x,x]*)
+(*intSubExp[x,x]*)
+(*\:8be5\:51fd\:6570\:53ea\:8981\:5b58\:5728a^(b x+c)\:ff0c\:5c31\:4f1a\:5904\:7406\:ff0c\:9664\:6b64\:4e4b\:5916\:ff0c\:4e0d\:4f1a\:5bf9\:5176\:5b83\:7c7b\:578b\:7684x\:6240\:7ed9\:7684\:4fe1\:606f\:7ed9\:51fa\:591a\:4f59\:7684\:5224\:65ad...*)
+(*intSubExp[E^x/(E^(x^2)+1),x]
+intSubExp[1/x,x]*)
+
