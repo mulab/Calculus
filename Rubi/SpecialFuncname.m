1e888499b70f5bbc211e9d2511761606965cbec1
Dejun Lin <buptldj@163.com>
2011-08-04 08:19:12 +0000

    [INT] mathematic自带的特殊函数名

---
+++ Rubi/SpecialFuncname.m
@@ -0,0 +1,20 @@
+(* ::Package:: *)
+
+(*\:7279\:6b8a\:51fd\:6570*)
+Erf
+Erfc
+Erfi
+ExpIntegralE
+ExpIntegralEi
+FresnelS
+FresnelC
+Gamma
+PolyGamma
+LogGamma
+SinhIntegral
+CoshIntegral
+LogIntegral
+ExpIntegralEi
+PolyLog
+ProductLog
+Zeta
