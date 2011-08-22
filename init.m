27a67a0874bae757e391503b5206ca4c0dd82ac5
Moony Chou <mengyu.chou@gmail.com>
2011-08-22 11:29:28 +0000

    [INT] 添加函数SinIntegral, CosIntegral, SinhIntegral, CoshIntegral, FresnelS, FresnelC的数值计算、DirectedInfinity、求导的定义

--- init.m
+++ init.m
@@ -6,9 +6,26 @@ CWD::usage="Current Work Directory";
 IntegrateU::usage="IntegrateU[f[x],x] returns the integral";
 
 
+(* Special Functions *)
+Unprotect[SinIntegral, CosIntegral, SinhIntegral, CoshIntegral, FresnelC, FresnelS];
+SinIntegral::usage = "SinIntegral[x] gives the sine integral
+Integrate[Sin[t]/t, {t, 0, x}].";
+CosIntegral::usage = "CosIntegral[x] gives the cosine integral
+EulerGamma + Log[x] + Integrate[(Cos[t] - 1)/t, {t, 0, x}].";
+SinhIntegral::usage = "SinhIntegral[x] gives the hyperbolic sine integral
+Integrate[Sinh[t]/t, {t, 0, x}].";
+CoshIntegral::usage = "CoshIntegral[x] gives the hyperbolic cosine integral
+EulerGamma + Log[x] + Integrate[(Cosh[t] - 1)/t, {t, 0, x}].";
+FresnelS::usage = "FresnelS[x] gives the Fresnel integral
+S[x] = Integrate[Sin[Pi t^2/2], {t, 0, x}].";
+FresnelC::usage = "FresnelC[x] gives the Fresnel integral
+C[x] = Integrate[Cos[Pi t^2/2], {t, 0, x}].";
+
+
 Begin["`Private`"]
 (*\:7531\:4e8eNotebookDirectory[]\:63d0\:53d6\:7684\:662f\:6700\:521d\:6267\:884c\:7684\:6587\:4ef6\:6240\:5728\:5b50\:76ee\:5f55\:ff0c\:6545\:624b\:5de5\:8bbe\:7f6eCWD\:6765\:786e\:5b9a\:5b50\:76ee\:5f55*)
 CWD=NotebookDirectory[];
+Get[CWD<>"\\IntegralFunctions.m"];
 (*Get[CWD<>"\\utility\\init.m"];*)
 Get[CWD<>"\\Rubi\\init.m"];
 Get[CWD<>"\\Albi\\init.m"];
@@ -17,6 +34,18 @@ Get[CWD<>"Integrate.m"];
 End[];
 
 
+(* Special Functions *)
+Attributes[SinIntegral] = {Listable, NumericFunction, ReadProtected};
+Attributes[CosIntegral] = {Listable, NumericFunction, ReadProtected};
+Attributes[SinhIntegral] = {Listable, NumericFunction, ReadProtected};
+Attributes[CoshIntegral] = {Listable, NumericFunction, ReadProtected};
+Attributes[FresnelS] = {Listable, NumericFunction, ReadProtected};
+Attributes[FresnelC] = {Listable, NumericFunction, ReadProtected};
+
+Protect[SinIntegral, CosIntegral, SinhIntegral, CoshIntegral, FresnelS, FresnelC];
+
+
+Remove[CWD];
 EndPackage[];
 
 
